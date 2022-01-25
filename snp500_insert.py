17432da14fb078af6ede08f71733acc339d8babb
MaxAcheson <maxacheson@gmail.com>
2022-01-25 12:13:34 -0800

    connect to database

--- snp500_insert.py
+++ snp500_insert.py
@@ -17,3 +17,20 @@ def insert_snp500_symbols(symbols):
         sector, currency, current_constituent, created, and last_updated
     """
     
+    # connect to securities master db
+    load_dotenv()
+    db_host = os.getenv('UW_SEC_MASTER_HOST')
+    db_user = os.getenv('UW_SEC_MASTER_USER')
+    db_pass = os.getenv('UW_SEC_MASTER_PASSWORD')
+    db_name = 'dev_sec_master'
+    db_port = '5432'
+
+    conn = psycopg2.connect(
+        database=db_name,
+        user=db_user,
+        password=db_pass,
+        host=db_host,
+        port=db_port
+    )
+
+    cur = conn.cursor()
