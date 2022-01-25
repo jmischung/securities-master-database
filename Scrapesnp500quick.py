67ed57f2e7f064091f599c21d80f86068041c06f
MaxAcheson <maxacheson@gmail.com>
2022-01-25 09:41:15 -0800

    Created scrape past and present snp500 scripts

---
+++ Scrapesnp500quick.py
@@ -0,0 +1,18 @@
+import os
+from datetime import datetime as dt
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import psycopg2
+
+df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+df[0]
+
+dfsnp500=df[0].copy()
+dfsnp500.drop(columns=['SEC filings','GICS Sub-Industry', 'Headquarters Location', 'CIK', 'Date first added'],axis=1,inplace=True)
+
+print(dfsnp500)
