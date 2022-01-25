7281ea47211b1a1bdb3add7d27ffeeeb22224c0f
MaxAcheson <maxacheson@gmail.com>
2022-01-25 11:53:31 -0800

    Built out obtain current snp500 function.

--- scrape_snp500_current.py
+++ scrape_snp500_current.py
@@ -7,13 +7,14 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import psycopg2
+from dotenv import load_dotenv
 
 # scrape and parse snp500 current constituents table
 def obtain_parse_wiki_snp500_current():
     """Download and parse the Wikipedia list of current
     SNP500 constituents using pandas.
     
-    Returns a list of tuples to add to the Securities
+    Returns a list of values to add to the Securities
     Master DB.
     
     Returns
@@ -23,18 +24,30 @@ def obtain_parse_wiki_snp500_current():
         insterted into the Securities master database
     """
 
+    # Store current time for "created_at" record
+    now = dt.utcnow()
+    
+    # Use pandas to downlaod list of snp500 
+    # companies into a dataframe
+    snp500_df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    snp500_current_df = snp500_df[0].copy()
+    snp500_current_df.drop(columns=['SEC filings',
+                                    'GICS Sub-Industry', 
+                                    'Headquarters Location', 
+                                    'CIK', 
+                                    'Founded',
+                                    'Date first added'],
+                            axis=1,
+                            inplace=True)
 
+    # Add instrument, currency and datetime to DataFrame.
+    snp500_current_df.insert(loc=1, column="instrument", value="stock")
 
+    snp500_current_df['currency'], snp500_current_df['current_constituent'], \
+        snp500_current_df['created'], snp500_current_df['last_updated'] = ['USD', 'true', now, now]
 
+    # Create a list from the values
+    # in the dataframe
+    symbols = snp500_current_df.values.tolist()
 
-df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-df[0]
-
-dfsnp500=df[0].copy()
-dfsnp500.drop(columns=['SEC filings','GICS Sub-Industry', 'Headquarters Location', 'CIK', 'Date first added'],axis=1,inplace=True)
-
-print(dfsnp500)
+    return symbols
