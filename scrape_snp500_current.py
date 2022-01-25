3f0791f4570fb8172f0c47a1b1b932392311588f
MaxAcheson <maxacheson@gmail.com>
2022-01-25 10:57:11 -0800

    changed file names, start scraping function for current and added documentation.

--- scrape_snp500_current.py
+++ scrape_snp500_current.py
@@ -1,9 +1,31 @@
+# scrape_snp500_current.py
+
+# Imports
 import os
 from datetime import datetime as dt
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import psycopg2
+
+# scrape and parse snp500 current constituents table
+def obtain_parse_wiki_snp500_current():
+    """Download and parse the Wikipedia list of current
+    SNP500 constituents using pandas.
+    
+    Returns a list of tuples to add to the Securities
+    Master DB.
+    
+    Returns
+    -------
+    'list'
+        A list of tuples that contain the values to be 
+        insterted into the Securities master database
+    """
+
+
+
+
 
 df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 pd.set_option('display.max_rows', None)
