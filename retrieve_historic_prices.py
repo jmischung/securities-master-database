0113e0052a76e69e0cea31de9903fc1d81aa0de2
emilybertani <emily.bertani.md@gmail.com>
2022-01-27 09:56:49 -0800

    Connecting to securities master db

--- retrieve_historic_prices.py
+++ retrieve_historic_prices.py
@@ -22,3 +22,19 @@ ALPHA_VANTAGE_TIME_SERIES_CALL = 'query?function=TIME_SERIES_DAILY'
 # Script operation
 TICKER_COUNT = 10  # Change this to adjust number of downloads, 505 records in `symbol` table as of 2021-09-02
 WAIT_TIME_IN_SECONDS = 5.0  # Adjust how frequently the API is called
+
+# Connect to securities master db
+db_host = os.getenv('UW_SEC_MASTER_HOST')
+db_user = os.getenv('UW_SEC_MASTER_USER')
+db_pass = os.getenv('UW_SEC_MASTER_PASSWORD')
+db_name = "dev_sec_master"
+db_port = "5432"
+
+conn = psycopg2.connect(
+    database=db_name,
+    user=db_user,
+    password=db_pass,
+    host=db_host,
+    port=db_port
+)
+
