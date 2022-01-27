0b3f5051a74b301d5fde19d135a41cb440838885
emilybertani <emily.bertani.md@gmail.com>
2022-01-27 09:35:28 -0800

    Adding Alpha Vantage keys

--- retrieve_historic_prices.py
+++ retrieve_historic_prices.py
@@ -14,3 +14,11 @@ import requests
 # Load variables into shell
 load_dotenv()
 
+# AlphaVantage variables
+ALPHA_VANTAGE_API_KEY = os.getenv('ALPHAVANTAGE_API_KEY')
+ALPHA_VANTAGE_BASE_URL = 'https://www.alphavantage.co'
+ALPHA_VANTAGE_TIME_SERIES_CALL = 'query?function=TIME_SERIES_DAILY'
+
+# Script operation
+TICKER_COUNT = 10  # Change this to adjust number of downloads, 505 records in `symbol` table as of 2021-09-02
+WAIT_TIME_IN_SECONDS = 5.0  # Adjust how frequently the API is called
